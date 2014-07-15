1fe026ecbf4316e9b9e490c76f46db1c8c49d56f
noon35 <noon35@gmail.com>
2014-07-15 19:17:38 -0400

    commit 3?

---
+++ CampFolderMachine/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.IO;
+
+namespace CampFolderMachine
+{
+    class Repository
+    {
+        private List<DirectoryInfo> rootFolders;
+
+        public List<DirectoryInfo> RootFolders
+        {
+            get { return rootFolders; }
+        }
+
+        public void AddRootFolder(DirectoryInfo newFolder)
+        {
+            rootFolders.Add(newFolder);
+        }
+    }
+}
