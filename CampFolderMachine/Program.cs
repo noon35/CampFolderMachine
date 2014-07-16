cfe82d51f260d9d887a377874f5de8b59667e0a9
noon35 <noon35@gmail.com>
2014-07-16 14:49:42 -0400

    code comments

--- CampFolderMachine/Program.cs
+++ CampFolderMachine/Program.cs
@@ -11,7 +11,7 @@ namespace CampFolderMachine
     {
         static void Main(string[] args)
         {
-            
+            //these are all the folder in g:\camps that have more camps within them
             Repository r = new Repository();
             r.AddRootFolder("G:\\camps");
             r.AddRootFolder("G:\\camps\\ada");
@@ -19,6 +19,7 @@ namespace CampFolderMachine
             r.AddRootFolder("G:\\Camps\\YMCA of Greater Hartford");
             r.AddRootFolder("G:\\Camps\\Lake Geneva Youth Camp & Conference Center");
 
+            //print them to the console to see that this is working
             foreach (System.IO.DirectoryInfo x in r.RootFolders)
             {
                 Console.WriteLine(x.FullName);
